e37e08243262e45093ad5826badbc0b7daa0b05f
mistydew <mistydew@qq.com>
2018-04-20 11:05:50 +0800

    add c/s json-rpc demo

---
+++ script/json_rpc.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+import json
+import requests
+import time
+
+def json_rpc(method, params):
+    payload = json.dumps({"method": method, "params": params})
+    response = requests.request("POST", url, data=payload, auth=auth)
+    return response.json()
+
+def getinfo():
+    return json_rpc("getinfo", [])
+
+def getbestblockinfo():
+    ret = json_rpc("getbestblockhash", [])
+    bestblockhash = ret["result"]
+    return json_rpc("getblock", [bestblockhash])
+
+def getdifficulty():
+    return json_rpc("getdifficulty", [])
+
+def getnetworkhashps():
+    return json_rpc("getnetworkhashps", [])
+
+def p2pool_func():
+    print("info:")
+    print(getinfo())
+    print("\nbestblockinfo:")
+    print(getbestblockinfo())
+    print("\ndifficulty:")
+    print(getdifficulty())
+    print("\nnetworkhashps:")
+    print(getnetworkhashps())
+
+if __name__ == "__main__":
+    url = "http://127.0.0.1:8332"
+    auth=("rpcuser", "rpcpassword")
+    while (1):
+        p2pool_func()
+        time.sleep(7)
